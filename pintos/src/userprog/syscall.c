04b6e9317195fdb07515658f40d4640f3f1cb671
Hayden Sartoris <hsartoris@gmail.com>
2018-04-13 16:31:30 -0400

    we got some syscalls bb

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,18 +3,55 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
-void
+	void
 syscall_init (void) 
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+	intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+	static void
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int* call = (int*)f->esp;
+	switch (*call) 
+	{
+		case SYS_HALT: //halt
+			shutdown_power_off();
+
+		case SYS_EXIT: //exit
+			//TODO: status
+			thread_exit();
+
+		case SYS_EXEC: //exec
+
+		case SYS_WAIT: //wait
+
+		case SYS_CREATE: //create
+
+		case SYS_REMOVE: //remove
+
+		case SYS_OPEN: //open
+
+		case SYS_FILESIZE: //filesize
+
+		case SYS_READ: //read
+
+		case SYS_WRITE: //write
+
+		case SYS_SEEK: //seek
+
+		case SYS_TELL: //tell
+
+		case SYS_CLOSE: //close
+
+		default:
+			printf("go fuck yourself\n");
+
+	}
+	printf ("system call %d!\n", *call);
+	thread_exit ();
 }
