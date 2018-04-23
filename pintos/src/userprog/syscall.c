2a7b26aaaa038870a943e057c6f536a3d9c05cf4
Cole Hollant <coleghollant@gmail.com>
2018-04-23 03:03:26 -0400

    mostly testing, wanted to throw in some comments lol

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,6 +12,7 @@
 #include "threads/vaddr.h"
 #endif
 
+#define virt_bottom ((int *)0x0804ba68) //maybe supposed to be a void *
 
 static void syscall_handler (struct intr_frame *);
 int get_file_length(int fd);
@@ -35,7 +36,7 @@ syscall_handler (struct intr_frame *f)
 	hex_dump(f->esp, f->esp, (int)(PHYS_BASE - f->esp), true);
 	// hopefully it works; just shut down after to avoid clutter
 	//shutdown_power_off();
-
+	printf("OH HOWDY%x\n", f->esp+1);
 	int* fd;
 	struct file* this_file;
 	const void* buff;
@@ -43,48 +44,56 @@ syscall_handler (struct intr_frame *f)
 	switch (*call) 
 	{
 		case SYS_HALT: //halt
+			printf("Halt!\n");
 			shutdown_power_off();
 
 		case SYS_EXIT: //exit
 			//TODO: status
+			printf("Exit!\n");
 			thread_exit();
 
 		case SYS_EXEC: //exec
-			
+			printf("Exec!\n");
 		case SYS_WAIT: //wait
-
+			printf("Wait!\n");
 		case SYS_CREATE: //create
-
+			printf("Create!\n");
 		case SYS_REMOVE: //remove
-
+			printf("Remove\n");
 		case SYS_OPEN: //open
-
+			printf("Remove\n");
 		case SYS_FILESIZE: //filesize NOT YET TESTED
+			printf("FileSize!\n");
 			fd = (int*)(f->esp + 1);	//theoretically the file
 			size = get_file_length(fd);
 			printf("%d\n", size);
 			break;
 		case SYS_READ: //read
+			printf("Read!\n");
 			buff[1234];	//likely garbagio
 			fd = (int*)(f->esp + 1);
 			size = get_file_length(fd);
 			read(fd, buff, size);
 		case SYS_WRITE: //write
+			printf("Write!\n");
 			fd = (int*)(f->esp + 1);
 			buff = (void*)(f->esp + 2);
 			size = (unsigned*)(f->esp + 3);	//seems sketchy and wrong <3 cole
-			printf("%d\n", *fd);
+			printf("%d\n", &fd);
+			printf("Hex of esp + 1 as int: %x\n", fd);
+			printf("Hex of esp at end: %x\n", f->esp+178);
+
 			if (*fd == 1)
 				putbuf(buff, *size);
 			break;
 		case SYS_SEEK: //seek
-
+			printf("Seek!\n");
 		case SYS_TELL: //tell
-
+			printf("Tell!\n");
 		case SYS_CLOSE: //close
-
+			printf("Close!\n");
 		default:
-			printf("aaaa\n");
+			printf("We love vim <3\n");
 	}
 	printf ("system call %d!\n", *call);
 	thread_exit ();
@@ -107,3 +116,22 @@ int read (int fd, void *buffer, unsigned size){
 	bytes_read = file_read(this_file, buffer, size);
 	return bytes_read;
 }
+
+
+
+/**************************************************************************
+ *Grabbed these boys off some assembly site ??
+ 
+ *cr2: control register
+ *error: error, you fool
+ *e$a-d$x: general purpose register
+ *esi & edi: index registers for DS and ES respectively
+ *esp: stack pointer B A B Y
+ *ebp: access data on the stack
+ *cs: code segment register, added to address during instruction "fetch"
+ *ds: data segment register: added to address when accessing a memory operand
+      that is not on the stack
+ *es: extra segment register, also used in special instructions that span
+      segments (ala: string copies)
+ *ss: stack segment, added to address during stack access
+ *************************************************************************/
